d1d8fdf527efeb5cae2c5d968e30b6969c42b766
Piotr Sekuła <piotr92345@o2.pl>
2023-12-01 11:59:10 +0100

    Not woring approach to part 2 #2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,21 @@
 fun main(args: Array<String>) {
     val fr = FileReader()
+    val day1 = Day1()
     println("Hello Advent Of Code 2023!")
 
     val allLines = fr.readAllLines("1.txt")
-    val result = Day1().calculateCalibrationValue(allLines)
-    println(result)
+
+    // Part 1
+//    val result = day1.calculateCalibrationValue(allLines)
+//    println(result)
+
+    // Part 2
+    val allLines2 = day1.fixLines(allLines)
+    val result2 = day1.calculateCalibrationValue(allLines2)
+
+    // 55431 too high
+    // 47191 -> wrong
+    // 47248 -> wrong
+    // 52418 -> wrong
+    println(result2)
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -1,7 +1,5 @@
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Day1Test {
 
@@ -16,5 +14,43 @@ class Day1Test {
         val input = listOf("1a2b3c", "45qqq", "rfv09")
         val result = Day1().calculateCalibrationValue(input)
         assertEquals(67, result)
+    }
+
+    @Test
+    fun shouldReplaceLetterNumbers() {
+        val result = Day1().replaceTextWithNumbersInLine("oneabc2deffour")
+        assertEquals("1abc2def4", result)
+
+        val result2 = Day1().replaceTextWithNumbersInLine("eightwothree")
+        assertEquals("8wo3", result2)
+
+        val result3 = Day1().replaceTextWithNumbersInLine("qd8fourvmvgmssixsix8oneighttps")
+        assertEquals("qd84vmvgmssixsix8on8tps", result3)
+
+        val result4 = Day1().replaceTextWithNumbersInLine("onetwoabc2deffour")
+        assertEquals("1twoabc2def4", result4)
+
+        val result5 = Day1().replaceTextWithNumbersInLine("qd8fourvmvgmssixsix8oneighttps")
+        assertEquals("qd84vmvgmssixsix8on8tps", result5)
+    }
+
+    @Test
+    fun shouldFixAllLinesAndCalculateCalibration() {
+        val d1 = Day1()
+        val allLines = listOf(
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+            "lpbjvpbtdfvtxtdvkpjs7qrvddkzmjtlqtg",
+            "qd8fourvmvgmssixsix8oneighttps"
+        )
+
+        val result = d1.fixLines(allLines)
+        val number = d1.calculateCalibrationValue(result)
+        assertEquals(376, number)
     }
 }
